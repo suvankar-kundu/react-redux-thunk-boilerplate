35e5ed34fd02c1d8bca227e6b54ce43138b82ba6
Suvankar Kundu <suvo.kundu26@gmail.com>
2019-08-25 20:06:21 +0530

    build coniguration change

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-
 import Main from "./Main";
 import configureStore from "./store";
 import { Provider } from "react-redux";
